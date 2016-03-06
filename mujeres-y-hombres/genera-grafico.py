ce5fad2dad0ecb651bab45bc4356c2a441df130c
David Charte <fdavidcl@outlook.com>
2016-03-06 20:31:45 +0100

    Cálculo de algunos datos numéricos para las páginas

--- mujeres-y-hombres/genera-grafico.py
+++ mujeres-y-hombres/genera-grafico.py
@@ -11,6 +11,7 @@ archivo_salida = "../index.html"
 churro_commpleto = ""
 icono = {}
 dicc_churros = {}
+dicc_stats = {}
 
 
 def crea_url(nombre):
@@ -64,6 +65,8 @@ with open(archivo_datos, 'r') as datos:
 
         churro_commpleto = churro_commpleto + churro_hombres + churro_mujeres
 
+        dicc_stats[fila[1]] = "Del total de {0} matriculados: <span class='hombres'>{1}</span> hombres (<span class='hombres'>{2:.2f} %</span>), <span class='mujeres'>{3}</span> mujeres (<span class='mujeres'>{4:.2f}</span> %)".format(fila[4], fila[2], float(fila[2])/int(fila[4])*100, fila[3], float(fila[3])/int(fila[4])*100)
+
 
 
 seleccion = crea_combo(dicc_churros)
@@ -78,6 +81,7 @@ for pag in dicc_churros:
     contenido = contenido.replace("[REPLACE_TITULO]", pag)
     contenido = contenido.replace("[REPLACE_MENU]", seleccion)
     contenido = contenido.replace("[REPLACE_CONTENIDO]", dicc_churros[pag])
+    contenido = contenido.replace("[REPLACE_STATS]", dicc_stats[pag])
 
     with open(nombre_pagina, 'w') as pagina:
         pagina.write(contenido)
@@ -87,6 +91,7 @@ with open(archivo_salida, 'w') as pagina:
 
     pagina_completa = pagina_completa.replace("[REPLACE_TITULO]", "Todas las carreras")
     pagina_completa = pagina_completa.replace("[REPLACE_MENU]", seleccion)
+    pagina_completa = pagina_completa.replace("[REPLACE_STATS]", "")
 #    pagina_completa = pagina_completa.replace("[REPLACE_CONTENIDO]", churro_commpleto)
 
     pagina.write(pagina_completa)
